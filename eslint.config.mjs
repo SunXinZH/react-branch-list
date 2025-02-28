2e0f971656170b707a0f59fb1f2b7c41bbaa62a0
SunXin <xin.sun.zh@gmail.com>
2025-02-28 16:39:27 +0800

    fix eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
+  pluginReact.configs.flat.recommended
 ];
